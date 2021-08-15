986925d5a4341deb97acbbc77a86ba7aa75dc901
Nicholas Berg <nicholas.bai.berg@gmail.com>
2021-08-16 08:22:36 +1200

    Added content from video recording

---
+++ FirstApp.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template("page.html")
+
+@app.route('/<name>')
+def hello_name(name):
+    return f"Hello {name}, are you a student in COMPSCI 235? If so you're cool!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
